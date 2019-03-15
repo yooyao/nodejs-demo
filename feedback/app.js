0b2468589d22477e5c394e4ed56f730346ce90d6
412108364@qq.com <412108364@qq.com>
2019-03-15 21:44:50 +0800

    添加了一个不是很美观的post提交方法，并且手动解析了查询字符串格式的请求体

--- feedback/app.js
+++ feedback/app.js
@@ -32,7 +32,7 @@ var comments = [
 //简写方式：
 http
 	.createServer(function (req, res){
-		console.log('req---------------',req.url);
+		// console.log('req---------------',req);
 		//url.parse()方法，解析URL成对象，
 		//第二个参数设为true表示将url中返回的query查询字符串解析成对象的结构，
 		//否则就是将query返回为没有解析也没有解码的字符串
@@ -84,6 +84,27 @@ http
 			res.statusCode = 302;
 			res.setHeader('Location','/');
 			res.end();
+		}else if(pathname == '/postComment'){
+			var data = '';
+			var dataObj = [];
+			//2.注册data事件接收数据（每当收到一段表单提交的数据，该方法会执行一次）
+			req.on('data', function (chunk) {
+				// chunk 默认是一个二进制数据，和 data 拼接会自动 toString
+				data += chunk;
+				console.log(data);
+				data = data.split("&");
+
+				for(let value of data){
+					var messArr = value.split("=");
+					dataObj[messArr[0]] = messArr[1];
+				}
+				console.log('dataObj-------',dataObj)
+				comments.unshift(dataObj);
+				// 页面重定向到首页
+				res.statusCode = 302;
+				res.setHeader('Location','/');
+				res.end();
+		});
 		}else{
 			res.setHeader("Content-Type","text/plain;charset=utf-8")
 			return res.end("这个路径不可以哦")
@@ -91,5 +112,5 @@ http
 				
 	})//返回的是server对象所以直接用连续操作
 	.listen(5000, function (){
-		console.log("running...")
+		console.log("running...in 5000");
 	})
